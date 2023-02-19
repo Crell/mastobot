fe33d9d04cb01bd7ce1d95348213186ca20d4fff
Larry Garfield <larry@garfieldtech.com>
2023-02-19 14:18:49 -0600

    Improve test coverage.

---
+++ tests/ConnectionFactoryTest.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Crell\Mastobot;
+
+use Crell\Serde\SerdeCommon;
+use PHPUnit\Framework\TestCase;
+
+class ConnectionFactoryTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function stuff(): void
+    {
+        $this->expectException(MissingAccountDefinition::class);
+
+        $f = new ConnectionFactory(new Config(appName: 'Test'), new SerdeCommon());
+
+        $f->getConnection('fake');
+    }
+}
